85332f8e6466d86352494b3c69a2ba1d93713da7
Sameer Singh <sameersingh2704@gmail.com>
2020-04-24 15:49:54 +0530

    v 0.3
    Only for 3rd year CSE Branch

--- rkgitlms.py
+++ rkgitlms.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 
 
-user_name=input("Enter your username")
-user_pass=input("Enter your password")
-class_section=input("Enter your class and section like:3D or 3C")
+user_name=input("Enter your username: ")
+user_pass=input("Enter your password: ")
+class_section=input("Enter your class and section (like 3D or 3C): ")
 req_url="http://rkgitlms.in/login/index.php"
 
 driver=webdriver.Chrome()
@@ -20,18 +20,22 @@ def authorization(user_name,user_pass):
     driver.find_element_by_id("loginbtn").click()
 
 
-def cse_3d():
-    driver.get("http://rkgitlms.in/mod/bigbluebuttonbn/view.php?id=231")
+def joinclass(class_section):
+    if(class_section=="3D"):
+        driver.get("http://rkgitlms.in/mod/bigbluebuttonbn/view.php?id=231")
+    elif(class_section=="3C"):
+        driver.get("http://rkgitlms.in/mod/bigbluebuttonbn/view.php?id=230")
+    elif(class_section=="3A"):
+        driver.get("http://rkgitlms.in/mod/bigbluebuttonbn/view.php?id=228")
+    elif(class_section=="3B"):
+        driver.get("http://rkgitlms.in/mod/bigbluebuttonbn/view.php?id=229")
+    else:
+        print("invalid class")
+        exit()
+    
     driver.find_element_by_id("join_button_input").click()
-def cse_3c():
-    driver.get("http://rkgitlms.in/mod/bigbluebuttonbn/view.php?id=230")
-    driver.find_element_by_id("join_button_input").click()
+
 
 login_page(req_url)
 authorization(user_name,user_pass)
-if(class_section=="3D"):
-    cse_3d()
-elif(class_section=="3C"):
-    cse_3c()
-else:
-    print("invalid class")
+joinclass(class_section)
